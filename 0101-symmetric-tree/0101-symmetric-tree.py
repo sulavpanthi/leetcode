9267d511662755778cd8a44682bbfa46c933a3a7
Sulav Panthi <43372413+sulavpanthi@users.noreply.github.com>
2024-07-21 08:03:30 +0545

    Time: 39 ms (43.26%), Space: 16.6 MB (11.1%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -8,13 +8,30 @@ class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         if not root: return False
 
-        def check_symmetry(left, right):
-            print(f"\n\nleft is {left} and \nright is {right}")
+        # # Approach 1: Using recursive approach
+        # def check_symmetry(left, right):
+        #     print(f"\n\nleft is {left} and \nright is {right}")
+        #     if not left and not right:
+        #         return True
+        #     if not left or not right:
+        #         return False
+        #     if not left.val == right.val:
+        #         return False
+        #     return check_symmetry(left.left, right.right) and check_symmetry(left.right, right.left)
+        # return check_symmetry(root.left, root.right)
+
+        # Approach 2: using iterative approach
+        from collections import deque
+        queue = deque([(root.left, root.right)])
+        while queue:
+            left, right = queue.popleft()
             if not left and not right:
-                return True
+                continue
             if not left or not right:
                 return False
-            if not left.val == right.val:
+            if left.val != right.val:
                 return False
-            return check_symmetry(left.left, right.right) and check_symmetry(left.right, right.left)
-        return check_symmetry(root.left, root.right)
+            queue.append((left.left, right.right))
+            queue.append((left.right, right.left))
+        return True
+            

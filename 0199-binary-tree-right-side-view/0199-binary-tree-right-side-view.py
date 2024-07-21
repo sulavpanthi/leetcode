40ba80f3ceaa2c0e66b2f20b043c7528127f3e1c
Sulav Panthi <43372413+sulavpanthi@users.noreply.github.com>
2024-07-21 15:38:52 +0545

    Time: 31 ms (88.14%), Space: 16.5 MB (64.03%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,16 +6,38 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root: return []
+        
+        # # Approach 1: Using recursion
+        # result = []
+        # max_level = -1
+        # def traversal(node, level):
+        #     nonlocal result, max_level
+        #     if not node:
+        #         return
+        #     if level > max_level:
+        #         result.append(node.val)
+        #         max_level += 1
+        #     traversal(node.right, level+1)
+        #     traversal(node.left, level+1)
+        # traversal(root, 0)
+        # return result
+
+
+        # Approach 2: Using iteration
+        from collections import deque
+        queue = deque([(root, 0)])
         result = []
         max_level = -1
-        def traversal(node, level):
-            nonlocal result, max_level
-            if not node:
-                return
-            if level > max_level:
-                result.append(node.val)
-                max_level += 1
-            traversal(node.right, level+1)
-            traversal(node.left, level+1)
-        traversal(root, 0)
+        while queue:
+            n = len(queue)
+            for _ in range(n):
+                node, level = queue.popleft()
+                if level > max_level:
+                    result.append(node.val)
+                    max_level = level
+                if node.right:
+                    queue.append((node.right, level + 1))
+                if node.left:
+                    queue.append((node.left, level + 1))
         return result

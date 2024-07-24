48d61e1463c9b3d21813e786f73738b259c778e5
Sulav Panthi <43372413+sulavpanthi@users.noreply.github.com>
2024-07-24 22:58:32 +0545

    Time: 82 ms (59.57%), Space: 20.2 MB (89.21%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -5,6 +5,7 @@
 #         self.left = None
 #         self.right = None
 
+from collections import deque
 class Codec:
 
     def serialize(self, root):
@@ -13,15 +14,30 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
+
+        # # Approach 1: Using dfs
+        # result = []
+        # def dfs(node):
+        #     if not node:
+        #         result.append("X")
+        #         return
+        #     result.append(str(node.val))
+        #     dfs(node.left)
+        #     dfs(node.right)
+        # dfs(root)
+        # return ",".join(result)
+
+        # Approach 2: Using level order traversal
+        queue = deque([root])
         result = []
-        def dfs(node):
+        while queue:
+            node = queue.popleft()
             if not node:
                 result.append("X")
-                return
-            result.append(str(node.val))
-            dfs(node.left)
-            dfs(node.right)
-        dfs(root)
+            else:
+                result.append(str(node.val))
+                queue.append(node.left)
+                queue.append(node.right)
         return ",".join(result)
         
 
@@ -31,19 +47,43 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        def dfs():
-            value = values.pop(0)
-            if value == 'X':
-                return None
-            root = TreeNode(val = value)
-            root.left = dfs()
-            root.right = dfs()
-            return root
 
+        # # Approach 1: Using dfs
+        # def dfs():
+        #     value = values.pop(0)
+        #     if value == 'X':
+        #         return None
+        #     root = TreeNode(val = value)
+        #     root.left = dfs()
+        #     root.right = dfs()
+        #     return root
+
+        # values = data.split(",")
+        # root = dfs()
+        # return root
+
+        # Approach 2: Using level order traversal
+        if not data:
+            return None
         values = data.split(",")
-        root = dfs()
+        if values[0] == 'X':
+            return None
+        root = TreeNode(val = int(values[0]))
+        queue = deque([root])
+        i = 1
+        while queue:
+            node = queue.popleft()
+            if values[i] != 'X':
+                node.left = TreeNode(val = int(values[i]))
+                queue.append(node.left)
+            i += 1
+
+            if values[i] != 'X':
+                node.right = TreeNode(val = int(values[i]))
+                queue.append(node.right)
+            i += 1
         return root
-        
+
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()

3f6c8ca7707b201833399358b506e7d6c1a32ab7
Sulav Panthi <43372413+sulavpanthi@users.noreply.github.com>
2024-07-24 16:07:30 +0545

    Time: 44 ms (97.29%), Space: 21.6 MB (92.62%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -6,16 +6,43 @@
 #         self.right = right
 from collections import deque
 class Solution:
+    
+    
     def countNodes(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        result = 0
-        queue = deque([root])
-        while queue:
-            node = queue.popleft()
-            result += 1
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-        return result
+        
+        # # Approach 1: Brute force
+        # result = 0
+        # queue = deque([root])
+        # while queue:
+        #     node = queue.popleft()
+        #     result += 1
+        #     if node.left:
+        #         queue.append(node.left)
+        #     if node.right:
+        #         queue.append(node.right)
+        # return result
+
+        # Approach 2: Optimal
+        def left_height(node):
+            height = 0
+            while node:
+                node = node.left
+                height += 1
+            return height
+
+        def right_height(node):
+            height = 0
+            while node:
+                node = node.right
+                height += 1
+            return height
+
+        left_height = left_height(root)
+        right_height = right_height(root)
+        if left_height == right_height:
+            return (1<<left_height) - 1 # 2^left_height-1
+        return 1+self.countNodes(root.left)+self.countNodes(root.right)
+
+        

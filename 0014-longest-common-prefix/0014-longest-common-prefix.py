15702a2784d89f3c477b95b22a233f4fbf962251
Sulav Panthi <43372413+sulavpanthi@users.noreply.github.com>
2024-07-26 14:49:30 +0545

    Time: 38 ms (56.18%), Space: 16.7 MB (23.38%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,11 +1,32 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        pointer = 0
-        min_length = min([len(s) for s in strs])
-        for _ in range(min_length):
-            ch = strs[0][pointer]
-            for s in strs[1:]:
-                if not s[pointer] == ch:
-                    return strs[0][:pointer]
-            pointer += 1
-        return strs[0][:pointer]
+
+        # # Approach 1: Using pointer
+        # pointer = 0
+        # min_length = min([len(s) for s in strs])
+        # for _ in range(min_length):
+        #     ch = strs[0][pointer]
+        #     for s in strs[1:]:
+        #         if not s[pointer] == ch:
+        #             return strs[0][:pointer]
+        #     pointer += 1
+        # return strs[0][:pointer]
+
+        
+        
+        # Approach 2: Using binary search approach
+
+        def is_common_prefix(length):
+            prefix = strs[0][:length]
+            return all(s.startswith(prefix) for s in strs)
+
+        low = 0
+        high = min_length = min([len(s) for s in strs])
+        while low<=high:
+            mid = (low+high) // 2
+            if is_common_prefix(mid):
+                low = mid + 1
+            else:
+                high = mid - 1
+        return strs[0][:(low+high)//2]
+            

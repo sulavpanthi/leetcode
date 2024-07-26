35fe2f7f01f859aba4f25eb6b6c6c49de7679abb
Sulav Panthi <43372413+sulavpanthi@users.noreply.github.com>
2024-07-26 10:19:35 +0545

    Time: 127 ms (5.6%), Space: 16.9 MB (17.14%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        
+        # using sliding window approach
+        result = []
+        s = s.strip()
+        p1, p2 = len(s) - 1, len(s) - 1
+        while p1 >= 0:
+            print("\n\np1 and p2 at start", p1, p2)
+
+            # find the word and append to the result
+            while s[p2] != " " and p2 >= 0:
+                p2 -= 1
+            result.append(s[p2+1:p1+1])
+
+            print("resutl after append", result, p1, p2)
+
+            # remove all whitespaces
+            while s[p2] == " ":
+                p2 -= 1
+
+            p1 = p2
+
+            print("p1 and p2 at last", p1, p2)
+        return " ".join(result)

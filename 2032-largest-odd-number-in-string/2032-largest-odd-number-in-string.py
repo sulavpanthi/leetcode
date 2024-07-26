c1f5fd1341ec256c886306850afa2f1878246201
Sulav Panthi <43372413+sulavpanthi@users.noreply.github.com>
2024-07-26 13:12:10 +0545

    Time: 574 ms (5%), Space: 17.8 MB (41.47%) - LeetHub

---
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.py
@@ -0,0 +1,40 @@
+class Solution:
+
+    # # Approach 1: Brute force (Generating all subsequences)
+
+    # def substring(self, s, i, res):
+    #     if i == len(s):
+    #         # to filter out the empty substring
+    #         if len(res) > 0:
+    #             self.result.append(int("".join(list(res))))
+    #         return
+    #     res.append(s[i])
+    #     self.substring(s, i+1, res)
+    #     # use .pop() here to ensure that last added element is removed
+    #     # while using .remove() might remove another element instead of recently added element e.g in case of duplicate elements already in the array
+    #     res.pop()
+    #     # res.remove(s[i])
+    #     self.substring(s, i+1, res)
+
+    # def largestOddNumber(self, num: str) -> str:
+    #     self.result = []
+    #     self.substring(num, 0, [])
+
+    #     # Filter all odd numbers
+    #     odd_ones = [x for x in self.result if x % 2 == 1]
+    #     if not odd_ones:
+    #         return ""
+
+    #     return str(max(odd_ones))
+
+
+
+
+    # Approach 2: Optimal
+
+    def largestOddNumber(self, num: str) -> str:
+        largest = float("-inf")
+        for i in range(len(num)):
+            if int(num[i]) % 2 == 1:
+                largest = num[:i+1]
+        return "" if largest == float("-inf") else str(largest)
